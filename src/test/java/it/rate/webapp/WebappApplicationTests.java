e757e4f72909496169fd18bf176568b5272d8a9b
Matěj Frnka <mat.frnka@gmail.com>
2023-12-04 09:37:59 +0100

    init

---
+++ src/test/java/it/rate/webapp/WebappApplicationTests.java
@@ -0,0 +1,11 @@
+package it.rate.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
