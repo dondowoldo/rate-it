93cedfc5a8ca3f81116011c544da9f09aa6d28f6
Ben <Benedikt.Vitek@gmail.com>
2023-12-11 11:47:21 +0100

    created basic controllers and todo.html (for compilation sake)

---
+++ src/main/java/it/rate/webapp/controllers/InterestController.java
@@ -0,0 +1,11 @@
+package it.rate.webapp.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/interests")
+public class InterestController {
+}
---
+++ src/main/java/it/rate/webapp/controllers/MainController.java
@@ -0,0 +1,10 @@
+package it.rate.webapp.controllers;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class MainController {
+}
