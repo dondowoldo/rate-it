c9bebd61e391064849c39c3ae5b214c879089600
Ben <Benedikt.Vitek@gmail.com>
2023-12-11 15:27:05 +0100

    updated InterestAdminController

--- src/main/java/it/rate/webapp/controllers/InterestAdminController.java
+++ src/main/java/it/rate/webapp/controllers/InterestAdminController.java
@@ -3,9 +3,7 @@ package it.rate.webapp.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/{interestId}/admin")
@@ -14,12 +12,27 @@ public class InterestAdminController {
 
 
     @GetMapping("/edit")
-    public String editPage(Model model) {
+    public String editPageView(Model model) {
 
-        //todo: add model attribute(Interest)
-        //todo: return page customization
+        //todo: add model attribute(Interest) according to id
+        //todo: return page customization/create template
         return "todo";
     }
+
+    @PutMapping("/edit")
+    public String editPage() {
+        //todo: update Interest object according to input changes
+        //todo: redirect to GET interest page
+        return "todo";
+    }
+
+    @GetMapping("/users")
+    public String editUsersPage() {
+        //todo: load list of users related to Interest
+        //todo: users template - form to invite users
+        return "todo";
+    }
+
 
 
 }
