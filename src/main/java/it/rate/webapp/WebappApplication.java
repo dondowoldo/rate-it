2279da4ed0b494e256d1cf53125d0f6b2dbc7eda
Martin Hübner <marthubner@gmail.com>
2023-12-11 14:38:29 +0100

    implement ComandLineRunner, inject repositoriesÅ

--- src/main/java/it/rate/webapp/WebappApplication.java
+++ src/main/java/it/rate/webapp/WebappApplication.java
@@ -1,12 +1,28 @@
 package it.rate.webapp;
 
+import it.rate.webapp.repositories.*;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebappApplication {
+@AllArgsConstructor
+public class WebappApplication implements CommandLineRunner {
+  private CriterionRepository criterionRepository;
+  private InterestRepository interestRepository;
+  private PlaceRepository placeRepository;
+  private RatingRepository ratingRepository;
+  private RoleRepository roleRepository;
+  private UserRepository userRepository;
+  private VoteRepository voteRepository;
 
   public static void main(String[] args) {
     SpringApplication.run(WebappApplication.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+
   }
 }
