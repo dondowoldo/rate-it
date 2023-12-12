0fc59f2e9d1da8ae2068a143ad0a4d7d94d0146d
Ben <Benedikt.Vitek@gmail.com>
2023-12-12 13:36:11 +0100

    semi working search bar done

---
+++ src/main/resources/static/scripts/index.js
@@ -0,0 +1,40 @@
+let suggestionsData = [];
+
+window.addEventListener('load', () => {
+    fetch('/getAllSuggestions')
+        .then(response => response.json())
+        .then(data => {
+            suggestionsData = data;
+        })
+        .catch(error => {
+            console.error('Error fetching suggestions:', error);
+        });
+});
+
+function getSuggestions(query) {
+    const suggestionList = document.getElementById('suggestionList');
+    suggestionList.innerHTML = '';
+
+    const filteredSuggestions = suggestionsData.filter(suggestion =>
+        suggestion.toLowerCase().includes(query.toLowerCase())
+    );
+
+    filteredSuggestions.forEach(suggestion => {
+        const li = document.createElement('li');
+        li.textContent = suggestion;
+        li.addEventListener('click', () => {
+            document.getElementById('search').value = suggestion;
+            document.getElementById('suggestions').style.display = 'none';
+        });
+        suggestionList.appendChild(li);
+    });
+
+    document.getElementById('suggestions').style.display = 'block';
+}
+
+document.addEventListener('click', function (e) {
+    const suggestions = document.getElementById('suggestions');
+    if (!e.target.matches('#search') && !e.target.matches('#suggestions *')) {
+        suggestions.style.display = 'none';
+    }
+});
