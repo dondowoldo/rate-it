59bf26fe6509ae832a490311fdb22debc62b8812
Ben <Benedikt.Vitek@gmail.com>
2023-12-12 19:07:48 +0100

    deleted forgotten import..

--- src/main/java/it/rate/webapp/services/MainService.java
+++ src/main/java/it/rate/webapp/services/MainService.java
@@ -6,7 +6,6 @@ import it.rate.webapp.repositories.InterestRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
