6814104a37e07d05b8108e25a0d357a97d64b89b
Daniel Valasek <daniel.valasek@yahoo.com>
2023-12-13 23:15:38 +0100

    RIB-22: add role-based access + testing endpoints

--- src/main/java/it/rate/webapp/config/security/SecurityConfig.java
+++ src/main/java/it/rate/webapp/config/security/SecurityConfig.java
@@ -3,28 +3,29 @@ package it.rate.webapp.config.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
   @Bean
   SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
     http.csrf(AbstractHttpConfigurer::disable);
+    http.headers(header -> header.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
     http.authorizeHttpRequests(
             authRequests ->
                 authRequests
-                    .requestMatchers("/users/signup")
+                    .requestMatchers("/users/signup", "/h2-console/**")
                     .permitAll()
                     .anyRequest()
                     .authenticated())
-        .formLogin(form -> form
-                .loginPage("/users/login")
-                .defaultSuccessUrl("/")
-                .permitAll())
+        .formLogin(form -> form.loginPage("/users/login").defaultSuccessUrl("/").permitAll())
         .httpBasic(Customizer.withDefaults());
     return http.build();
   }
