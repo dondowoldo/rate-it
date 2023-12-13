5c61dc76591cdc17326309f46cbb57de7556444c
Jan Benda <bendajan@centrum.cz>
2023-12-13 18:30:18 +0100

    style fix

--- src/main/java/it/rate/webapp/services/CreateInterestService.java
+++ src/main/java/it/rate/webapp/services/CreateInterestService.java
@@ -14,37 +14,33 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+
 @Service
 @AllArgsConstructor
 public class CreateInterestService {
 
-    private InterestRepository interestRepository;
-    private CriterionRepository criterionRepository;
-    private UserRepository userRepository;
+  private InterestRepository interestRepository;
+  private CriterionRepository criterionRepository;
+  private UserRepository userRepository;
 
-    public Interest save(String name, String description, List<String> receivedCriteria) {
-        List<Criterion> criteria = receivedCriteria.stream()
-                .map(c -> Criterion.builder().name(c).build())
-                .toList();
+  public Interest save(String name, String description, List<String> receivedCriteria) {
+    List<Criterion> criteria =
+        receivedCriteria.stream().map(c -> Criterion.builder().name(c).build()).toList();
 
-        criterionRepository.saveAll(criteria);
+    criterionRepository.saveAll(criteria);
 
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        Optional<AppUser> currentUser = userRepository.findByEmail(authentication.getName());
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    Optional<AppUser> currentUser = userRepository.findByEmail(authentication.getName());
 
-        if (currentUser.isEmpty()) {
-            throw new RuntimeException(); // todo: probably something more specific
-        }
-
-        Interest i = Interest.builder()
-            .name(name)
-            .description(description)
-            .criteria(criteria)
-            .build();
-
-        i.getRoles().add(new Role(currentUser.get(), i, Role.RoleType.ADMIN));
-        // todo: ADMIN to be changed to CREATOR after merge
-
-        return interestRepository.save(i);
+    if (currentUser.isEmpty()) {
+      throw new RuntimeException(); // todo: probably something more specific
     }
+
+    Interest i = Interest.builder().name(name).description(description).criteria(criteria).build();
+
+    i.getRoles().add(new Role(currentUser.get(), i, Role.RoleType.ADMIN));
+    // todo: ADMIN to be changed to CREATOR after merge
+
+    return interestRepository.save(i);
+  }
 }
