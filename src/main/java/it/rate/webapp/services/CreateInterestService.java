51f5de140fdbb7c767a320d11b947a8a15120892
Jan Benda <bendajan@centrum.cz>
2023-12-13 17:28:57 +0100

    modified interest save method to include current user's ID and assign him ADMIN role

--- src/main/java/it/rate/webapp/services/CreateInterestService.java
+++ src/main/java/it/rate/webapp/services/CreateInterestService.java
@@ -1,13 +1,19 @@
 package it.rate.webapp.services;
 
+import it.rate.webapp.models.AppUser;
 import it.rate.webapp.models.Criterion;
 import it.rate.webapp.models.Interest;
+import it.rate.webapp.models.Role;
 import it.rate.webapp.repositories.CriterionRepository;
 import it.rate.webapp.repositories.InterestRepository;
+import it.rate.webapp.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -15,6 +21,7 @@ public class CreateInterestService {
 
     private InterestRepository interestRepository;
     private CriterionRepository criterionRepository;
+    private UserRepository userRepository;
 
     public Interest save(String name, String description, List<String> receivedCriteria) {
         List<Criterion> criteria = receivedCriteria.stream()
@@ -23,13 +30,21 @@ public class CreateInterestService {
 
         criterionRepository.saveAll(criteria);
 
-        Interest i = Interest.builder()
-                .name(name)
-                .description(description)
-                .criteria(criteria)
-                // todo: add user from session as admin to roles
-                .build();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Optional<AppUser> currentUser = userRepository.findByEmail(authentication.getName());
 
+        if (currentUser.isEmpty()) {
+            throw new RuntimeException(); // todo: probably something more specific
+        }
+
+        Interest i = Interest.builder()
+            .name(name)
+            .description(description)
+            .criteria(criteria)
+            .build();
+
+        i.getRoles().add(new Role(currentUser.get(), i, Role.RoleType.ADMIN));
+        // todo: ADMIN to be changed to CREATOR after merge
 
         return interestRepository.save(i);
     }
