11a6d2a8ffdf0831b98d22a0ccbb4261fe5fb6b4
Ben <Benedikt.Vitek@gmail.com>
2023-12-14 16:56:12 +0100

    style changes

---
+++ src/test/java/it/rate/webapp/services/InterestServiceTest.java
@@ -0,0 +1,29 @@
+package it.rate.webapp.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+import it.rate.webapp.repositories.InterestRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class InterestServiceTest {
+
+  @Autowired
+  MockMvc mockMvc;
+
+  @MockBean
+  InterestRepository interestRepository;
+  @Test
+  void getAllSuggestionDtos() {
+
+    when(interestRepository.findAll().thanReturn())
+
+  }
+}
