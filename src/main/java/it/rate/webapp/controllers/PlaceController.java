fd6cacb5a54b3a9a0ec71625e81c2f9a5bc394bf
Ben <Benedikt.Vitek@gmail.com>
2023-12-14 19:11:23 +0100

    added case of missing placeId in GET /placeid/edit

--- src/main/java/it/rate/webapp/controllers/PlaceController.java
+++ src/main/java/it/rate/webapp/controllers/PlaceController.java
@@ -59,7 +59,7 @@ public class PlaceController {
       Principal principal,
       HttpServletResponse response) {
 
-    if(placeService.findById(placeId).isEmpty()) {
+    if (placeService.findById(placeId).isEmpty()) {
       response.setStatus(HttpServletResponse.SC_NOT_FOUND);
       model.addAttribute("message", "This place doesn't exist");
     }
