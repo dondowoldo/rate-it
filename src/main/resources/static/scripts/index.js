e240ea344ee33d91a54f33361216cbfb13a56e92
Daniel Valasek <daniel.valasek@yahoo.com>
2023-12-30 00:26:58 +0100

    RIB-20: Index page basic styling with limited functionality

--- src/main/resources/static/scripts/index.js
+++ src/main/resources/static/scripts/index.js
@@ -15,45 +15,131 @@ document.addEventListener('DOMContentLoaded', () => {
     loadAllSuggestions();
 });
 
+// function loadAllSuggestions() {
+//     const suggestionList = document.getElementById('suggestionList');
+//     suggestionList.innerHTML = '';
+//
+//     suggestionsData.forEach(suggestion => {
+//         const li = document.createElement('li');
+//         li.textContent = `${suggestion.name} (Likes: ${suggestion.rating})`;
+//         li.addEventListener('click', () => {
+//             window.location.href = `/interests/${suggestion.id}`;
+//         });
+//         suggestionList.appendChild(li);
+//     });
+//
+//     document.getElementById('suggestions').style.display = 'block';
+// }
+
 function loadAllSuggestions() {
-    const suggestionList = document.getElementById('suggestionList');
-    suggestionList.innerHTML = '';
+    const suggestionContainer = document.querySelector('#suggestionList');
+    suggestionContainer.innerHTML = '';
+
 
     suggestionsData.forEach(suggestion => {
-        const li = document.createElement('li');
-        li.textContent = `${suggestion.name} (Likes: ${suggestion.rating})`;
-        li.addEventListener('click', () => {
-            window.location.href = `/interests/${suggestion.id}`;
-        });
-        suggestionList.appendChild(li);
+        const div = document.createElement('div');
+        div.classList.add('interest-card');
+
+        const link = document.createElement('a');
+        link.classList.add('interest-card-link');
+        link.href = `/interests/${suggestion.id}`;
+
+        const innerDiv = document.createElement('div');
+
+        const imgWrapper = document.createElement('div');
+        imgWrapper.classList.add('img-wrapper');
+
+        const interestTitle = document.createElement('div');
+        interestTitle.classList.add('interest-title');
+
+        const paragraph = document.createElement('p');
+        paragraph.classList.add('interest-name');
+        paragraph.textContent = `${suggestion.name} (${suggestion.rating})`;
+
+        interestTitle.appendChild(paragraph);
+        innerDiv.appendChild(imgWrapper);
+        innerDiv.appendChild(interestTitle);
+        link.appendChild(innerDiv);
+        div.appendChild(link);
+
+        suggestionContainer.appendChild(div);
     });
 
     document.getElementById('suggestions').style.display = 'block';
 }
 
+// function getSuggestions(query) {
+//     const suggestionList = document.getElementById('suggestionList');
+//     suggestionList.innerHTML = '';
+//
+//     if (!isEmptyOrSpaces(query)) {
+//
+//         const filteredSuggestions = suggestionsData.filter(suggestion =>
+//             suggestion.name.toLowerCase().includes(query.toLowerCase())
+//         );
+//
+//         filteredSuggestions.forEach(suggestion => {
+//             const li = document.createElement('li');
+//             li.textContent = `${suggestion.name} (Rating: ${suggestion.rating})`;
+//             li.addEventListener('click', () => {
+//                 window.location.href = `/interests/${suggestion.id}`;
+//             });
+//             suggestionList.appendChild(li);
+//         });
+//
+//     } else {
+//         loadAllSuggestions();
+//     }
+//
+//
+//     document.getElementById('suggestions').style.display = 'block';
+// }
+
 function getSuggestions(query) {
-    const suggestionList = document.getElementById('suggestionList');
-    suggestionList.innerHTML = '';
+    const suggestionContainer = document.querySelector('#suggestionList');
+    suggestionContainer.innerHTML = '';
 
     if (!isEmptyOrSpaces(query)) {
-
         const filteredSuggestions = suggestionsData.filter(suggestion =>
             suggestion.name.toLowerCase().includes(query.toLowerCase())
         );
 
         filteredSuggestions.forEach(suggestion => {
-            const li = document.createElement('li');
-            li.textContent = `${suggestion.name} (Rating: ${suggestion.rating})`;
-            li.addEventListener('click', () => {
+            const div = document.createElement('div');
+            div.classList.add('interest-card');
+
+            const link = document.createElement('a');
+            link.classList.add('interest-card-link');
+            link.href = `/interests/${suggestion.id}`;
+
+            const innerDiv = document.createElement('div');
+
+            const imgWrapper = document.createElement('div');
+            imgWrapper.classList.add('img-wrapper');
+
+            const interestTitle = document.createElement('div');
+            interestTitle.classList.add('interest-title');
+
+            const paragraph = document.createElement('p');
+            paragraph.classList.add('interest-name');
+            paragraph.textContent = `${suggestion.name} (${suggestion.rating})`;
+
+            interestTitle.appendChild(paragraph);
+            innerDiv.appendChild(imgWrapper);
+            innerDiv.appendChild(interestTitle);
+            link.appendChild(innerDiv);
+            div.appendChild(link);
+
+            suggestionContainer.appendChild(div);
+
+            // Click event listener for redirection
+            div.addEventListener('click', () => {
                 window.location.href = `/interests/${suggestion.id}`;
             });
-            suggestionList.appendChild(li);
         });
-
     } else {
         loadAllSuggestions();
     }
-
 
     document.getElementById('suggestions').style.display = 'block';
 }
