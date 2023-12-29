cd848df6a83816d97f55b46dcbdc23d6b45aa477
Daniel Valasek <daniel.valasek@yahoo.com>
2023-12-29 16:37:56 +0100

    RIB-52: editUsers method integration tests

--- src/test/java/it/rate/webapp/controllers/InterestAdminControllerIntegrationTest.java
+++ src/test/java/it/rate/webapp/controllers/InterestAdminControllerIntegrationTest.java
@@ -1,42 +1,139 @@
 package it.rate.webapp.controllers;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 import it.rate.webapp.BaseIntegrationTest;
+import it.rate.webapp.services.ManageInterestService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
 @AutoConfigureMockMvc
 class InterestAdminControllerIntegrationTest extends BaseIntegrationTest {
 
-    @Autowired private MockMvc mockMvc;
+  @Autowired private MockMvc mockMvc;
+  @MockBean private ManageInterestService manageInterestService;
 
-    @Test
-    void editPageView() {
-      }
+  @Test
+  void editPageView() {}
 
-    @Test
-    void editPage() {
-      }
+  @Test
+  void editPage() {}
 
-    @Test
-    void editUsersPage() {
-      }
+  @Test
+  @WithMockUser(
+      username = "alfonz@alfonz.cz",
+      authorities = {"USER", "ROLE_VOTER_1"})
+  void editUsersReturnsForbiddenForVoter() throws Exception {
+    Long interestId = 1L;
+    mockMvc
+        .perform(get("/interests/" + interestId + "/admin/users"))
+        .andExpect(status().isForbidden())
+        .andReturn();
 
-    @Test
-    void removeVoter() {
-      }
+    verify(manageInterestService, times(0)).getUsersByRole(interestId);
+  }
 
-    @Test
-    void rejectApplicant() {
-      }
+  @Test
+  @WithMockUser(
+      username = "alfonz@alfonz.cz",
+      authorities = {"USER", "ROLE_APPLICANT_1"})
+  void editUsersReturnsForbiddenForApplicant() throws Exception {
+    Long interestId = 1L;
+    mockMvc
+        .perform(get("/interests/" + interestId + "/admin/users"))
+        .andExpect(status().isForbidden())
+        .andReturn();
 
-    @Test
-    void acceptApplicant() {
-      }
+    verify(manageInterestService, times(0)).getUsersByRole(interestId);
+  }
+
+  @Test
+  @WithMockUser(
+      username = "alfonz@alfonz.cz",
+      authorities = {"USER"})
+  void editUsersReturnsForbiddenForNoRolesUser() throws Exception {
+    Long interestId = 1L;
+    mockMvc
+        .perform(get("/interests/" + interestId + "/admin/users"))
+        .andExpect(status().isForbidden())
+        .andReturn();
+
+    verify(manageInterestService, times(0)).getUsersByRole(interestId);
+  }
+
+  @Test
+  @WithMockUser(
+      username = "alfonz@alfonz.cz",
+      authorities = {"USER", "ROLE_CREATOR_2"})
+  void editUsersReturnsForbiddenForCreatorOfDifferentInterest() throws Exception {
+    Long interestId = 1L;
+    mockMvc
+        .perform(get("/interests/" + interestId + "/admin/users"))
+        .andExpect(status().isForbidden())
+        .andReturn();
+
+    verify(manageInterestService, times(0)).getUsersByRole(interestId);
+  }
+
+  @Test
+  @WithMockUser(
+      username = "alfonz@alfonz.cz",
+      authorities = {"ADMIN"})
+  void editUsersReturnsErrorForNonExistentInterest() throws Exception {
+    Long interestId = Long.MAX_VALUE;
+    mockMvc
+        .perform(get("/interests/" + interestId + "/admin/users"))
+        .andExpect(status().isNotFound())
+        .andReturn();
+
+    verify(manageInterestService, times(0)).getUsersByRole(interestId);
+  }
+
+  @Test
+  @WithMockUser(
+      username = "alfonz@alfonz.cz",
+      authorities = {"USER", "ROLE_CREATOR_1"})
+  void editUsersReturnsSuccessStatusForCreator() throws Exception {
+    Long interestId = 1L;
+    mockMvc
+        .perform(get("/interests/" + interestId + "/admin/users"))
+        .andExpect(status().isOk())
+        .andReturn();
+
+    verify(manageInterestService, times(1)).getUsersByRole(interestId);
+  }
+
+  @Test
+  @WithMockUser(
+      username = "alfonz@alfonz.cz",
+      authorities = {"ADMIN"})
+  void editUsersReturnsSuccessForAdmin() throws Exception {
+    Long interestId = 1L;
+    mockMvc
+        .perform(get("/interests/" + interestId + "/admin/users"))
+        .andExpect(status().isOk())
+        .andReturn();
+
+    verify(manageInterestService, times(1)).getUsersByRole(interestId);
+  }
+
+  @Test
+  void removeVoter() {}
+
+  @Test
+  void rejectApplicant() {}
+
+  @Test
+  void acceptApplicant() {}
 }
