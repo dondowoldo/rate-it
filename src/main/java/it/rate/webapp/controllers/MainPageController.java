6f32683ee56b361a693aa23ae28a5bc7c3aee0ca
Daniel Valasek <daniel.valasek@yahoo.com>
2024-01-01 15:41:05 +0100

    RIB-51 delete redundant css

--- src/main/java/it/rate/webapp/controllers/MainPageController.java
+++ src/main/java/it/rate/webapp/controllers/MainPageController.java
@@ -4,7 +4,6 @@ import it.rate.webapp.services.InterestService;
 import it.rate.webapp.services.UserService;
 import java.security.Principal;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
