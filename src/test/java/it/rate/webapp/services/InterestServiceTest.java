d7f92001239102472e034f4f4a4d4ee8d7423511
Jan Benda <bendajan@centrum.cz>
2024-01-03 15:13:28 +0100

    RIB-37: style hotfix

--- src/test/java/it/rate/webapp/services/InterestServiceTest.java
+++ src/test/java/it/rate/webapp/services/InterestServiceTest.java
@@ -20,85 +20,84 @@ import static org.mockito.Mockito.*;
 
 class InterestServiceTest extends BaseTest {
 
-    @MockBean InterestRepository interestRepository;
-    @MockBean CriterionRepository criterionRepository;
+  @MockBean InterestRepository interestRepository;
+  @MockBean CriterionRepository criterionRepository;
 
-    @Autowired InterestService interestService;
+  @Autowired InterestService interestService;
 
-    List<Criterion> criteria = new ArrayList<>();
-    Criterion c1;
-    Criterion c2;
-    Interest i;
+  List<Criterion> criteria = new ArrayList<>();
+  Criterion c1;
+  Criterion c2;
+  Interest i;
 
-    @BeforeEach
-    void createCriteria() {
-        c1 = Criterion.builder().name("c1").build();
-        c2 = Criterion.builder().name("c2").build();
-        criteria.add(c1);
-        criteria.add(c2);
-        criterionRepository.saveAll(criteria);
+  @BeforeEach
+  void createCriteria() {
+    c1 = Criterion.builder().name("c1").build();
+    c2 = Criterion.builder().name("c2").build();
+    criteria.add(c1);
+    criteria.add(c2);
+    criterionRepository.saveAll(criteria);
 
-        i = Interest.builder().criteria(criteria).build();
-    }
+    i = Interest.builder().criteria(criteria).build();
+  }
 
-    @Test
-    void saveInterestWithNewCriteria() {
-        List<Criterion> updatedCriteria = new ArrayList<>();
-        Criterion c3 = new Criterion();
-        Criterion c4 = new Criterion();
-        updatedCriteria.add(c3);
-        updatedCriteria.add(c4);
+  @Test
+  void saveInterestWithNewCriteria() {
+    List<Criterion> updatedCriteria = new ArrayList<>();
+    Criterion c3 = new Criterion();
+    Criterion c4 = new Criterion();
+    updatedCriteria.add(c3);
+    updatedCriteria.add(c4);
 
-        when(interestRepository.save(any())).thenAnswer(i -> i.getArgument(0));
-        when(criterionRepository.saveAll(any())).thenAnswer(i -> i.getArgument(0));
+    when(interestRepository.save(any())).thenAnswer(i -> i.getArgument(0));
+    when(criterionRepository.saveAll(any())).thenAnswer(i -> i.getArgument(0));
 
-        criterionRepository.deleteByNameAndInterestId(c1.getName(), i.getId());
-        criterionRepository.deleteByNameAndInterestId(c2.getName(), i.getId());
-        criterionRepository.saveAll(updatedCriteria);
+    criterionRepository.deleteByNameAndInterestId(c1.getName(), i.getId());
+    criterionRepository.deleteByNameAndInterestId(c2.getName(), i.getId());
+    criterionRepository.saveAll(updatedCriteria);
 
-        i.setCriteria(updatedCriteria);
-        Interest savedInterest = interestService.save(i);
+    i.setCriteria(updatedCriteria);
+    Interest savedInterest = interestService.save(i);
 
-        assertSame(savedInterest.getCriteria(), updatedCriteria);
-        assertFalse(criterionRepository.findAll().contains(c1));
-        assertFalse(criterionRepository.findAll().contains(c2));
-    }
+    assertSame(savedInterest.getCriteria(), updatedCriteria);
+    assertFalse(criterionRepository.findAll().contains(c1));
+    assertFalse(criterionRepository.findAll().contains(c2));
+  }
 
-    @Test
-    void saveInterestWithChangedCriteria() {
-        List<Criterion> updatedCriteria = new ArrayList<>();
-        Criterion c3 = new Criterion();
-        Criterion c4 = new Criterion();
-        updatedCriteria.add(c2);
-        updatedCriteria.add(c3);
-        updatedCriteria.add(c4);
+  @Test
+  void saveInterestWithChangedCriteria() {
+    List<Criterion> updatedCriteria = new ArrayList<>();
+    Criterion c3 = new Criterion();
+    Criterion c4 = new Criterion();
+    updatedCriteria.add(c2);
+    updatedCriteria.add(c3);
+    updatedCriteria.add(c4);
 
-        when(interestRepository.save(any())).thenAnswer(i -> i.getArgument(0));
-        when(criterionRepository.saveAll(any())).thenAnswer(i -> i.getArgument(0));
+    when(interestRepository.save(any())).thenAnswer(i -> i.getArgument(0));
+    when(criterionRepository.saveAll(any())).thenAnswer(i -> i.getArgument(0));
 
+    criterionRepository.deleteByNameAndInterestId(c1.getName(), i.getId());
+    criterionRepository.saveAll(updatedCriteria);
 
-        criterionRepository.deleteByNameAndInterestId(c1.getName(), i.getId());
-        criterionRepository.saveAll(updatedCriteria);
+    i.setCriteria(updatedCriteria);
+    Interest editedInterest = interestRepository.save(i);
 
-        i.setCriteria(updatedCriteria);
-        Interest editedInterest = interestRepository.save(i);
+    assertEquals(3, editedInterest.getCriteria().size());
+    assertSame(editedInterest.getCriteria(), updatedCriteria);
+    assertFalse(criterionRepository.findAll().contains(c1));
+  }
 
-        assertEquals(3, editedInterest.getCriteria().size());
-        assertSame(editedInterest.getCriteria(), updatedCriteria);
-        assertFalse(criterionRepository.findAll().contains(c1));
-    }
+  @Test
+  void saveNewInterest() {
+    when(interestRepository.save(any())).thenAnswer(i -> i.getArgument(0));
+    when(criterionRepository.saveAll(any())).thenAnswer(i -> i.getArgument(0));
 
-    @Test
-    void saveNewInterest() {
-        when(interestRepository.save(any())).thenAnswer(i -> i.getArgument(0));
-        when(criterionRepository.saveAll(any())).thenAnswer(i -> i.getArgument(0));
+    List<Criterion> savedCriteria = criterionRepository.saveAll(criteria);
+    Interest savedInterest = interestService.save(i);
 
-        List<Criterion> savedCriteria = criterionRepository.saveAll(criteria);
-        Interest savedInterest = interestService.save(i);
+    assertEquals(criteria.size(), savedCriteria.size());
+    assertSame(savedInterest.getCriteria(), savedCriteria);
 
-        assertEquals(criteria.size(), savedCriteria.size());
-        assertSame(savedInterest.getCriteria(), savedCriteria);
-
-        verify(interestRepository, times(1)).save(same(i));
-    }
+    verify(interestRepository, times(1)).save(same(i));
+  }
 }
