8c2c50f4e0504fe1dd7f841aae929f192601ba88
Ben <Benedikt.Vitek@gmail.com>
2024-01-07 20:14:16 +0100

    created imageUpload.js, updated place.page so uppy can be loaded there. Updated controller

--- src/main/java/it/rate/webapp/controllers/ImageController.java
+++ src/main/java/it/rate/webapp/controllers/ImageController.java
@@ -1,13 +1,10 @@
 package it.rate.webapp.controllers;
 
-import it.rate.webapp.services.RoleService;
-import it.rate.webapp.services.UserService;
 import java.io.IOException;
 import java.nio.file.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,33 +13,23 @@ import org.springframework.web.multipart.MultipartFile;
 @Controller
 @RequiredArgsConstructor
 public class ImageController {
-    private final RoleService roleService;
-    private final UserService userService;
 
-    @GetMapping("interests/{interestId}/new-image")
-    @PreAuthorize("hasAnyAuthority(@permissionService.manageCommunity(#interestId))")
-    public String interestImageUploadView(@PathVariable Long interestId) {
+  @PostMapping("interests/{interestId}/places/{placeId}")
+  @PreAuthorize("hasAnyAuthority(@permissionService.ratePlace(#placeId))")
+  public String uploadPlaceImage(
+      @RequestParam("picture") MultipartFile file, @PathVariable Long interestId, @PathVariable Long placeId) {
+      
+      // Testing purposes
+      String testUploadDirectory =
+              "C:\\Users\\Bened\\OneDrive\\Plocha\\testImageFolder\\" + file.getOriginalFilename();
 
-        return "interest/imageUpload";
-    }
+      try {
+          Path pathToDirectory = Paths.get(testUploadDirectory);
+          file.transferTo(pathToDirectory);
+      } catch (IOException e) {
+          throw new RuntimeException(e);
+      }
 
-    @PostMapping("interests/{interestId}/new-image")
-    @PreAuthorize("hasAnyAuthority(@permissionService.manageCommunity(#interestId))")
-    public String uploadNewInterestImage(
-            @RequestParam("picture") MultipartFile file,
-            @PathVariable Long interestId) {
-
-        //So far for testing purposes
-        String testUploadDirectory = "C:\\Users\\Bened\\OneDrive\\Plocha\\testImageFolder\\" + file.getOriginalFilename();
-
-        try {
-            Path pathToDirectory = Paths.get(testUploadDirectory);
-            file.transferTo(pathToDirectory);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-
-        return "redirect:/interests/" + interestId;
-    }
+      return "redirect:/interests/" + interestId + "/places/" + placeId;
+  }
 }
--- src/main/resources/static/scripts/imageUpload.js
+++ src/main/resources/static/scripts/imageUpload.js
@@ -5,17 +5,36 @@ let dynamicEndpoint = window.location.pathname;
 const uppy = new Uppy({
     restrictions: {
         allowedFileTypes: ["image/*"],
-        maxFileSize:  10 * 1024 * 1024
+        maxFileSize: 10 * 1024 * 1024,
+
     }
 })
-    .use(Dashboard, {target: '#uppy', inline: true})
+    .use(Dashboard, {
+        target: '#uppy',
+        inline: false,
+        proudlyDisplayPoweredByUppy: false,
+        trigger: '#uppy-modal',
+        closeModalOnClickOutside: true,
+    })
     .use(XHRUpload, {
         endpoint: dynamicEndpoint,
         fieldName: 'picture',
-        formData: true
+        formData: true,
     })
     .use(Webcam, {
         target: Dashboard,
         showVideoSourceDropdown: true,
         modes: 'picture'
     })
+
+
+const uppyAddFilesList = document.querySelector('.uppy-Dashboard--modal .uppy-Dashboard-inner');
+
+if (uppyAddFilesList) {
+    uppyAddFilesList.style.top = '30%';
+    uppyAddFilesList.style.bottom = 'auto';
+}
+
+const customStyles = document.createElement('style');
+customStyles.innerHTML = '.uppy-Dashboard-AddFiles-title { display: none !important; }'
+document.head.appendChild(customStyles);
