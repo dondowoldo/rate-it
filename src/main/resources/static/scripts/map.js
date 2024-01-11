0aff956a52c7891b3ae1507ffdb90f2d5ecbd229
Martin Hübner <marthubner@gmail.com>
2024-01-11 11:50:04 +0100

    RIB-78: add ability to position and drag pin in place/form.html

--- src/main/resources/static/scripts/map.js
+++ src/main/resources/static/scripts/map.js
@@ -1,7 +1,17 @@
 let latlng = L.latLng([49.8037633, 15.4749126]);
-let map = L.map('map').setView(latlng, 14);
+let map = L.map('map').setView(latlng, 7);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+let marker;
+map.on("click", function (e) {
+    if (marker != null) {
+        marker.remove();
+    }
+    marker = new L.marker([e.latlng.lat, e.latlng.lng], {draggable: true, autoPan: true}).addTo(map);
+
+
+})
