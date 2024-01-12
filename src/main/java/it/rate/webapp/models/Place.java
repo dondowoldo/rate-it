7d248b90e47333b84c95d75eb8df18f87673384f
Martin Hübner <marthubner@gmail.com>
2024-01-12 18:36:24 +0100

    RIB-79: remove incorrectly used precission

--- src/main/java/it/rate/webapp/models/Place.java
+++ src/main/java/it/rate/webapp/models/Place.java
@@ -23,11 +23,11 @@ public class Place {
   private String description;
   private String address;
 
-  @Column(precision = 6, nullable = false)
+  @Column(nullable = false)
   @Range(min = -90, max = 90)
-  private Double latitude;
+  private double latitude;
 
-  @Column(precision = 6, nullable = false)
+  @Column(nullable = false)
   @Range(min = -180, max = 180)
   private Double longitude;
 
