3825090c27ced09c753d936f9d9dc51f26e7a03b
Ben <Benedikt.Vitek@gmail.com>
2024-01-15 17:17:01 +0100

    fixed picture loading using javascript on interest place. Combining thymeleaf fetching and direct call from JS. Pictures are now all loaded when first entered interest page so they are no new api calls every time user uses searchbar

--- src/main/resources/static/scripts/imageFetcher.js
+++ src/main/resources/static/scripts/imageFetcher.js
@@ -1,21 +1,47 @@
 const pictureContainer = document.getElementById('picture-container');
 
-const imageId = document.getElementById('picture-container').dataset.imageId;
+if (pictureContainer) {
+    const imageId = pictureContainer.dataset.imageId;
+    const apiUrl = '/api/v1/images/' + imageId;
 
-const apiUrl = '/api/v1/images/' + imageId;
+    fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.blob();
+        })
+        .then(blob => {
+            const imageUrl = URL.createObjectURL(blob);
+            pictureContainer.src = imageUrl;
+        })
+        .catch(error => {
+            console.error('Error fetching picture:', error);
+            pictureContainer.src = 'https://picsum.photos/500/300';
+        });
+} else {
+    function fetchImageUrl(place) {
+        const imageNames = place.imageNames;
 
-fetch(apiUrl)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
+        if (imageNames && imageNames.length > 0) {
+            const imageId = imageNames[0];
+            const apiUrl = '/api/v1/images/' + imageId;
+
+            return fetch(apiUrl)
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+                    return response.blob();
+                })
+                .then(blob => URL.createObjectURL(blob))
+                .catch(error => {
+                    console.error('Error fetching image:', error);
+                    return 'https://picsum.photos/400/300';
+                });
+        } else {
+            console.error('No imageNames found for the place');
+            return 'https://picsum.photos/400/300';
         }
-        return response.blob();
-    })
-    .then(blob => {
-        const imageUrl = URL.createObjectURL(blob);
-        pictureContainer.src = imageUrl;
-    })
-    .catch(error => {
-        console.error('Error fetching picture:', error);
-        pictureContainer.src = 'https://picsum.photos/500/300';
-    });
+    }
+}
