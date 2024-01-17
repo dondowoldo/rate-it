d30d3de28da61615ec3bef1319b814c0dd40754a
Jan Benda <bendajan@centrum.cz>
2024-01-17 19:51:53 +0100

    RIB-98: fixed default padding value, it should appear correctly on phones now

--- src/main/resources/static/scripts/sidebars.js
+++ src/main/resources/static/scripts/sidebars.js
@@ -1,13 +1,17 @@
 function updateStyles() {
     const container = document.querySelector('.container-fluid');
     const windowWidth = window.innerWidth;
-    const startWidth = 500;
-    const endWidth = window.screen.width;
-    let sidebarPercentage = (windowWidth - startWidth) / (endWidth - startWidth);
-    sidebarPercentage = Math.max(0, Math.min(sidebarPercentage, 1));
-    const actualSidebarWidth = sidebarPercentage * 35;
-    container.style.setProperty('--sidebar-width', `${actualSidebarWidth}vw`);
-    const paddingValue = Math.max(actualSidebarWidth, 0);
+
+    if (windowWidth > 499) {
+        const startWidth = 500;
+        const endWidth = window.screen.width;
+        let sidebarPercentage = (windowWidth - startWidth) / (endWidth - startWidth);
+        sidebarPercentage = Math.max(0, Math.min(sidebarPercentage, 1));
+        const actualSidebarWidth = sidebarPercentage * 35;
+        container.style.setProperty('--sidebar-width', `${actualSidebarWidth}vw`);
+        const paddingValue = Math.max(actualSidebarWidth, 0);
+    }
+
 }
 
 updateStyles();
