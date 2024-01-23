929a45df13930b7f403e7e87eabe5b218c99ba61
Ben <Benedikt.Vitek@gmail.com>
2024-01-23 14:26:56 +0100

    added thumbnailaitor dependency. Max upload file size changed to 5mb. When user uploads file larger than 1mb, size will be reduced

--- src/main/java/it/rate/webapp/services/GoogleImageService.java
+++ src/main/java/it/rate/webapp/services/GoogleImageService.java
@@ -1,10 +1,14 @@
 package it.rate.webapp.services;
 
+import static com.google.common.io.Files.getFileExtension;
+
 import com.google.api.client.http.FileContent;
 import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.model.File;
 import it.rate.webapp.exceptions.api.ApiServiceUnavailableException;
 import it.rate.webapp.models.Interest;
+import it.rate.webapp.utils.ImageEditor;
+import jakarta.validation.constraints.NotNull;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
@@ -25,16 +29,27 @@ public class GoogleImageService implements ImageService {
   private String UPLOAD_DIRECTORY;
 
   private final Drive driveService;
+  private final ImageEditor imageEditor;
 
 
   @Override
-  public String saveImage(MultipartFile image) throws IOException {
+  public String saveImage(@NotNull MultipartFile image) throws IOException { //todo: get principal
 
     FileContent mediaContent;
+
     String currentUser = SecurityContextHolder.getContext().getAuthentication().getName();
 
-    Path tempFilePath = Files.createTempFile("temp_" + currentUser, null);
+    String originalFileName = image.getOriginalFilename();
+
+    String fileExtension = getFileExtension(originalFileName);
+
+    if (fileExtension.isEmpty()) {
+      fileExtension = ".jpg";
+    }
+    Path tempFilePath = Files.createTempFile("temp_" + currentUser, "." + fileExtension);
     image.transferTo(tempFilePath);
+    imageEditor.reduceImageSize(tempFilePath);
+
     mediaContent = new FileContent("image/*", tempFilePath.toFile());
 
     String fileName = currentUser + "_" + UUID.randomUUID();
@@ -77,6 +92,7 @@ public class GoogleImageService implements ImageService {
     }
   }
 
+  @Override
   public byte[] getImageById(String imageId) {
 
     try (InputStream imageStream = driveService.files().get(imageId).executeMediaAsInputStream()) {
@@ -86,8 +102,4 @@ public class GoogleImageService implements ImageService {
     }
   }
 
-  @Override
-  public List<String> getImages(String imageId) {
-    return null;
-  }
 }
--- src/main/java/it/rate/webapp/utils/ImageEditor.java
+++ src/main/java/it/rate/webapp/utils/ImageEditor.java
@@ -1,2 +1,43 @@
-package it.rate.webapp.utils;public class ImageEditor {
+package it.rate.webapp.utils;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import net.coobird.thumbnailator.Thumbnails;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ImageEditor {
+
+  private final int MEGABYTE = 1024 * 1024;
+
+  
+  public void reduceImageSize(Path imagePath) throws IOException {
+    
+    long fileSizeBytes = Files.size(imagePath);
+
+    if (fileSizeBytes <= MEGABYTE) {
+      return;
+    }
+    
+    double reductionRate = calculateReductionRate(fileSizeBytes);
+
+    Thumbnails.of(imagePath.toFile())
+            .outputQuality(reductionRate)
+            .scale(1)
+            .toFile(imagePath.toFile());
+  }
+
+  public double calculateReductionRate(long fileSizeByte) {
+
+    if (fileSizeByte < 2 * MEGABYTE) {
+      return 0.5;
+    } else if (fileSizeByte < 3 * MEGABYTE) {
+      return 0.3;
+    } else if (fileSizeByte < 4 * MEGABYTE) {
+      return 0.25;
+    } else {
+      return 0.2;
+    }
+  }
 }
