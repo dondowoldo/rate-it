2c9bbe8735a2949f11a135fe8895e1abf12a1a0a
Ben <Benedikt.Vitek@gmail.com>
2024-01-24 10:59:58 +0100

    style changes

--- src/main/java/it/rate/webapp/utils/ImageEditor.java
+++ src/main/java/it/rate/webapp/utils/ImageEditor.java
@@ -11,25 +11,24 @@ public class ImageEditor {
 
   private final int MEGABYTE = 1024 * 1024;
 
-  
   public void reduceImageSize(Path imagePath) throws IOException {
-    
+
     long fileSizeBytes = Files.size(imagePath);
 
     double reductionRate = calculateReductionRate(fileSizeBytes);
 
     Thumbnails.of(imagePath.toFile())
-            .outputQuality(reductionRate)
-            .scale(1)
-            .toFile(imagePath.toFile());
+        .outputQuality(reductionRate)
+        .scale(1)
+        .toFile(imagePath.toFile());
   }
 
   private double calculateReductionRate(long fileSizeByte) {
 
-    if(fileSizeByte < MEGABYTE) {
+    if (fileSizeByte < MEGABYTE) {
       return 1.0;
     } else {
-      return (double) MEGABYTE /fileSizeByte;
+      return (double) MEGABYTE / fileSizeByte;
     }
   }
 }
