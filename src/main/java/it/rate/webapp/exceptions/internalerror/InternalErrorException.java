b1da9a73c67216119879528aa2c0003c7d02abb8
vdrfg <137323658+vdrfg@users.noreply.github.com>
2024-01-26 16:33:32 +0100

    Update InternalErrorException.java

--- src/main/java/it/rate/webapp/exceptions/internalerror/InternalErrorException.java
+++ src/main/java/it/rate/webapp/exceptions/internalerror/InternalErrorException.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
 public class InternalErrorException extends RuntimeException {
-
   public InternalErrorException() {
     super("Internal server error");
   }
