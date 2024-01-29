0532c5b4bbe7a6bcae9fa5dd7439827c8ab6b975
Jan Benda <bendajan@centrum.cz>
2024-01-29 04:20:21 +0100

    removed unused import

--- src/main/java/it/rate/webapp/config/security/SecurityConfig.java
+++ src/main/java/it/rate/webapp/config/security/SecurityConfig.java
@@ -10,7 +10,6 @@ import org.springframework.security.config.annotation.web.configurers.HeadersCon
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Configuration
 @EnableMethodSecurity
