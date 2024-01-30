57b5aed7b26da9ff7800171ffdc4b33921c82144
Jan Benda <bendajan@centrum.cz>
2024-01-30 20:47:52 +0100

    RIB-161: added follow function to HTML and added id to userDTO

--- src/main/java/it/rate/webapp/dtos/AppUserDTO.java
+++ src/main/java/it/rate/webapp/dtos/AppUserDTO.java
@@ -1,14 +1,12 @@
 package it.rate.webapp.dtos;
 
 import it.rate.webapp.models.AppUser;
-import it.rate.webapp.models.Interest;
 
-import java.util.List;
-
-public record AppUserDTO(String username, String bio, int followers, int follows) {
+public record AppUserDTO(Long id, String username, String bio, int followers, int follows) {
 
   public AppUserDTO(AppUser appUser) {
     this(
+        appUser.getId(),
         appUser.getUsername(),
         appUser.getBio(),
         appUser.getFollowers().size(),
