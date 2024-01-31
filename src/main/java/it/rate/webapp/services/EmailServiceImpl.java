fba324df0296950dc1a70e16fc43941f855cd0e6
Jan Benda <bendajan@centrum.cz>
2024-01-31 09:31:25 +0100

    removed unused imports

--- src/main/java/it/rate/webapp/services/EmailServiceImpl.java
+++ src/main/java/it/rate/webapp/services/EmailServiceImpl.java
@@ -2,8 +2,6 @@ package it.rate.webapp.services;
 
 import it.rate.webapp.dtos.EmailMessageDTO;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
