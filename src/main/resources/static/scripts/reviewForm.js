504e64180ccde4a9117356c8a271e4814299830b
Martin Hübner <marthubner@gmail.com>
2024-01-31 21:30:05 +0100

    RIB-163: add modal javascript handling

--- src/main/resources/static/scripts/reviewForm.js
+++ src/main/resources/static/scripts/reviewForm.js
@@ -12,12 +12,21 @@ function updateCharCountAndResize(maxLength) {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
+    const TIMEOUT_DURATION = 2000;
+
+    let previousReviewText = '';
 
     async function handleSubmit(event) {
         try {
             event.preventDefault();
 
             const reviewText = document.querySelector('#review-text-field').value;
+
+            if (!reviewText.trim() || reviewText === previousReviewText) {
+                displayMessage('Please enter a new review before submitting.', 'info');
+                return;  // Stop further execution if the review text is empty or the same
+            }
+
             const response = await fetch(`/api/v1/places/${placeId}/review`, {
                 method: 'POST',
                 body: reviewText,
@@ -33,9 +42,29 @@ document.addEventListener('DOMContentLoaded', function () {
             const responseData = await response.text();
             console.log(responseData);
 
+            displayMessage('Review sent successfully!', 'success');
+
+            setTimeout(function() {
+                $('#review-modal').modal('hide');
+            }, TIMEOUT_DURATION);
+
+            previousReviewText = reviewText;
+
         } catch (error) {
             console.error(error);
+
+            displayMessage('Error sending review. Please try again later.', 'danger');
         }
+    }
+
+    function displayMessage(message, type) {
+        const charCountElement = document.querySelector('#charCount');
+
+        charCountElement.innerHTML = `<div class="alert alert-${type}" role="alert">${message}</div>`;
+
+        setTimeout(function() {
+            charCountElement.innerHTML = '';
+        }, TIMEOUT_DURATION);
     }
 
     const form = document.getElementById("review-form");
