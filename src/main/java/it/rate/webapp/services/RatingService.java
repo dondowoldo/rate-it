607544be11654333ee1fe3a32beec7283dc236e1
Jan Benda <bendajan@centrum.cz>
2024-02-01 13:40:58 +0100

    RIB-161: reversed sorting on places ratings

--- src/main/java/it/rate/webapp/services/RatingService.java
+++ src/main/java/it/rate/webapp/services/RatingService.java
@@ -114,7 +114,7 @@ public class RatingService {
                             place ->
                                 RatingMapper.remapToUserRatedPlaceDTO(
                                     place.getKey(), place.getValue()))
-                        .sorted(Comparator.comparingDouble(UserRatedPlaceDTO::avgRating))
+                        .sorted(Comparator.comparingDouble(UserRatedPlaceDTO::avgRating).reversed())
                         .collect(Collectors.toList())))
         .sorted(Comparator.comparingInt(UserRatedInterestDTO::likes).reversed())
         .toList();
