bb010c76bc562d9e16692d285bf183365444883f
Jan Benda <bendajan@centrum.cz>
2024-02-01 17:31:26 +0100

    RIB-162: style hotfix

--- src/main/java/it/rate/webapp/repositories/RatingRepository.java
+++ src/main/java/it/rate/webapp/repositories/RatingRepository.java
@@ -13,12 +13,13 @@ public interface RatingRepository extends JpaRepository<Rating, RatingId> {
 
   List<Rating> findAllByAppUser(AppUser appUser);
 
-  @Query("SELECT DISTINCT r FROM Interest i " +
-          "LEFT JOIN i.criteria c " +
-          "LEFT JOIN i.places p " +
-          "LEFT JOIN c.ratings cr " +
-          "LEFT JOIN p.ratings pr " +
-          "LEFT JOIN Rating r ON r IN (cr, pr) " +
-          "WHERE r.appUser = :appUser AND i = :interest")
+  @Query(
+      "SELECT DISTINCT r FROM Interest i "
+          + "LEFT JOIN i.criteria c "
+          + "LEFT JOIN i.places p "
+          + "LEFT JOIN c.ratings cr "
+          + "LEFT JOIN p.ratings pr "
+          + "LEFT JOIN Rating r ON r IN (cr, pr) "
+          + "WHERE r.appUser = :appUser AND i = :interest")
   List<Rating> findAllByAppUserAndInterestId(AppUser appUser, Interest interest);
 }
